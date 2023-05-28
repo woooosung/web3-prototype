0f9b72980d40dfb8f340deb41de2c2a9379e5f7b
presenthee <khyunhii@gmail.com>
2023-05-28 21:52:20 +0900

    fix build

--- typescript-version/src/layouts/components/SurveyForm.js
+++ typescript-version/src/layouts/components/SurveyForm.js
@@ -19,7 +19,7 @@ const SurveyForm = () => {
 
   const submitUser = async () => {
     const cookieName = 'myCookie'
-    const cookieValue = ''
+    let cookieValue = ''
     const cookies = document.cookie.split(';')
     for (let i = 0; i < cookies.length; i++) {
       const cookie = cookies[i].trim()
